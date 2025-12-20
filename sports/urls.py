95a9a91fe73e4c1fb8e944c9a6123d10c4a2074f
firstaqila <saffana.firsta@gmail.com>
2025-12-20 11:44:16 +0700

    logic for flutter

--- sports/urls.py
+++ sports/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from sports.views import show_main, create_sport, show_sport, show_json, show_json_by_id, edit_sport, delete_sport, create_sport_entry_ajax, edit_sport_entry_ajax, delete_sport_entry_ajax
+from sports.views import show_main, create_sport, show_sport, show_json, show_json_by_id, edit_sport, delete_sport, create_sport_entry_ajax, edit_sport_entry_ajax, delete_sport_entry_ajax, proxy_image, create_sport_flutter, edit_sport_flutter, delete_sport_flutter
 
 app_name = 'sports'
 
@@ -14,4 +14,8 @@ urlpatterns = [
     path('create-sport-ajax/', create_sport_entry_ajax, name='create_sport_entry_ajax'),
     path('edit-sport-ajax/<uuid:id>/', edit_sport_entry_ajax, name='edit_sport_entry_ajax'),
     path('delete-sport-ajax/<uuid:id>/', delete_sport_entry_ajax, name='delete_sport_entry_ajax'),
+    path('proxy-image/', proxy_image, name='proxy_image'),
+    path('create-flutter/', create_sport_flutter, name='create_sport_flutter'),
+    path('edit-flutter/<uuid:id>/', edit_sport_flutter, name='edit_sport_flutter'),
+    path('delete-flutter/<uuid:id>/', delete_sport_flutter, name='delete_sport_flutter'),
 ]
